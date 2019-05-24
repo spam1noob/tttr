59d5ab36c64ca6709fd8b267c5641b061630052c
spam1noob <45920860+spam1noob@users.noreply.github.com>
2019-05-24 21:25:25 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("581513381073125396")
+setInterval(function() {
+channel.send(`walla ya wad ene 47en nase`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
